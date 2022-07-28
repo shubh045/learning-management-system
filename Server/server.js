783f7ad7aea8ddbc62b5cca2fde5846432d62bd8
shivansh singh <shivanshsingh05102000@gmail.com>
2022-07-28 15:27:51 +0530

    Connected to db

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,17 @@
-
+const mongoose = require('mongoose');
 
 const http = require('http');
+const dbUrl="mongodb+srv://Admin:VIPZb498zKAImRYQ@cluster0.zvzw94r.mongodb.net/?retryWrites=true&w=majority"
+const connectionParams={
+    useNewUrlParser:true,
+}
+
+mongoose.connect(dbUrl,connectionParams).then(()=>{
+    console.info("connectd to DB");
+})
+.catch((e)=>{
+    console.log("Error:",e);
+})
 
 let app = http.createServer((req, res) => {
 
@@ -10,5 +21,5 @@ let app = http.createServer((req, res) => {
 });
 
 
-app.listen(3000, '127.0.0.1');
+app.listen(3100, '127.0.0.1');
 console.log('Node server running on port 3000');
