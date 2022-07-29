4eb35187a369762b0e56fef972804c178e7b5726
Harsh24021999 <93443250+Harsh24021999@users.noreply.github.com>
2022-07-29 17:20:57 +0530

    Updated App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-// import Logo from "./component/Logo";
-// import Downcurve from "./component/Downcurve";
-import Apply from "./component/Applyleave/Apply";
 import Login from "./pages/Login";
 
 
 function App() {
   return (
     <>
-      {/* <Logo /> */}
-      {/* <Downcurve /> */}
-{/* <Apply/> */}
-    
   <Login/>
     </>
   );
