2d773d0d474628c8addb44a77e67341d65884e64
shubh045 <shubh6441@gmail.com>
2022-08-13 11:03:39 +0530

    updated leaveapply

--- Server/models/leaveapply.js
+++ Server/models/leaveapply.js
@@ -1,14 +1,18 @@
-const mongoose = require("mongoose");
+
+const mongoose = require("mongoose"); 
 
 const applyLeave = new mongoose.Schema(
   {
+    userEmail: {type: String},
+    userName: {type: String},
+    status: {type: String},
     dateApplied: {type: Date},
     leaveType: { type: String, required: true },
     fromDate: { type: Date, required: true },
     toDate: { type: Date, required: true },
     subject: { type: String, required: true },
     description: { type: String, required: true },
-    managerEmail: { type: String}
+    managerEmail: { type: Array},
   },
   { collation: {locale: "en_US", strength: 1} }
 );
