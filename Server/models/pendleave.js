674954f94a984fc372ebeee4759ffac150a8022d
shubh045 <shubh6441@gmail.com>
2022-08-13 11:02:07 +0530

    updated pendleave

--- Server/models/pendleave.js
+++ Server/models/pendleave.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const leavePend = new mongoose.Schema(
   {
+    userEmail: {type: String},
+    userName: {type: String},
     status: {type: String},
     dateApplied: {type: Date},
     leaveType: { type: String, required: true },
     fromDate: { type: Date, required: true },
     toDate: { type: Date, required: true },
     subject: { type: String, required: true },
-    description: { type: String, required: true }
+    description: { type: String, required: true }, 
   },
   { collation: "pendingleave" }
 ); 
